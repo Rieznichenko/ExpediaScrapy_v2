547540678308ae7d20565deb52bfcfe1192e890b
Fiedotov <Ihorfiedotov@gmail.com>
2024-04-11 21:15:26 +0300

    feat: fix server url

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ async function main() {
 
   console.log("--------Request Sent-------");
   const response = await axios.post(
-    "http://52.23.160.89:3128/api/v1/expedia/result",
+    "http://13.48.232.122:3128/api/v1/expedia/result",
     body
   );
 
